9f9eb98d8efe692905393eaf5f91854c187d28e9
Carlos Vinicius <viniciusdev.26@gmail.com>
2026-05-09 13:06:58 -0300

    chore: update next config

--- apps/site/next.config.ts
+++ apps/site/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '*.r2.cloudflarestorage.com' },
       { protocol: 'https', hostname: '*.r2.dev' },
-      { protocol: 'https', hostname: 'cvinicius.com' },
+      { protocol: 'https', hostname: 'father-public.cvinicius.com' },
     ],
   },
 };
